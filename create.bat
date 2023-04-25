0604999bd06c7146d7bc3555c3337bbd3eb22cc3
Donny <1021001390@qq.com>
2023-04-25 12:18:53 +0800

    Init Project

---
+++ create.bat
@@ -0,0 +1,14 @@
+docker rm -f ranger
+docker run -d ^
+-it ^
+-p 6080:6080 ^
+--name ranger ^
+--net my-network --ip 172.20.1.72 ^
+--link mysql8:mysql8 ^
+-v %cd%\java:/usr/share/java ^
+-v %cd%\sh\start.sh:/root/start.sh ^
+-v %cd%\ranger-2.0.0-admin:/opt/ranger-2.0.0-admin ^
+-v %cd%\conf:/etc/ranger/admin/conf ^
+--privileged=true ^
+openjdk:8 ^
+bash -c "/root/start.sh; bash"
---
+++ sh/start.sh
@@ -0,0 +1,35 @@
+cd /opt/ranger-2.0.0-admin
+
+# if [ -s `which lsof`];
+# then
+# sed -i 's#http://deb.debian.org#https://mirrors.ustc.edu.cn#g' /etc/apt/sources.list
+# sed -i 's|security.debian.org/debian-security|mirrors.ustc.edu.cn/debian-security|g' /etc/apt/sources.list
+
+# apt update && apt install lsof
+# fi
+
+# useradd -M solr
+# rm -rf /opt/solr/bin/solr-*.pid
+# mkdir -p /var/log/solr/ranger_audits
+# chown -R solr:solr /var/log/solr/ranger_audits
+
+# useradd -M ranger -p "ranger"
+
+# /opt/ranger-2.0.0-admin/contrib/solr_for_audit_setup/setup.sh
+
+# /opt/solr-8.6.3/ranger_audit_server/scripts/start_solr.sh
+
+if [ -s `which python`];
+then
+sed -i 's#http://deb.debian.org#https://mirrors.ustc.edu.cn#g' /etc/apt/sources.list
+sed -i 's|security.debian.org/debian-security|mirrors.ustc.edu.cn/debian-security|g' /etc/apt/sources.list
+
+apt update && apt install -y python
+fi
+
+RANGER_ADMIN_CONF=/opt/ranger-2.0.0-admin
+/opt/ranger-2.0.0-admin/setup.sh
+/opt/ranger-2.0.0-admin/set_globals.sh
+ranger-admin start
+
+tail -f ews/logs/catalina.out
